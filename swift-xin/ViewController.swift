961d9323ec10731a6d2f81c5c3808de4decd879e
Dylan Walker Brown <dylan@Einstein.local>
2014-06-04 20:05:44 -0700

    Initial Commit

---
+++ swift-xin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-xin
+//
+//  Created by Dylan Walker Brown on 6/4/14.
+//  Copyright (c) 2014 Dylan Walker Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
