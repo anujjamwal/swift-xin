6a13634d30652864016a66e02a8891a93260058c
Dylan Walker Brown <dylan@Einstein.local>
2014-06-04 20:19:38 -0700

    Included Objective-C MailCore 2 external framework using 'bridging-header'.

--- swift-xin/ViewController.swift
+++ swift-xin/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Instantiate an IMAP session.
+        let session: MCOIMAPSession = MCOIMAPSession()
     }
 
     override func didReceiveMemoryWarning() {
